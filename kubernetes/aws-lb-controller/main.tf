266785f465c47d1e106aa18004e08629c1801f58
Hannes Doyle <hannes.doyle@tv4.se>
2021-11-18 16:15:50 +0100

    Add loadbalancer controller from AWS

---
+++ kubernetes/aws-lb-controller/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.4.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.2.0"
+    }
+  }
+}
+
+resource "kubernetes_service_account" "this" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = var.role_arn
+    }
+  }
+}
+
+resource "helm_release" "aws_lb" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+
+  set {
+    name  = "clusterName"
+    value = var.name
+  }
+  set {
+    name  = "region"
+    value = var.region
+  }
+  set {
+    name  = "vpcId"
+    value = var.vpc
+  }
+  set {
+    name  = "image.repository"
+    value = "${var.container_images[var.region]}/aws-load-balancer-controller"
+  }
+  set {
+    name  = "serviceAccount.create"
+    value = false
+  }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+}
