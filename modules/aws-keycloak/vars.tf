dc79aa419892578392baed3a8822257f5b0f4976
Tobias Widen <tobias.widen@tv4.se>
2022-06-30 13:55:11 +0200

    Keycloak docs

--- modules/aws-keycloak/vars.tf
+++ modules/aws-keycloak/vars.tf
@@ -1,5 +1,6 @@
 variable "certificate_arn" {
   type = string
+  description = "ARN of the ACM Certificate to be attached to ALB HTTPS Listener"
 }
 
 variable "min_containers" {
@@ -17,6 +18,7 @@ variable "max_containers" {
 variable "auto_scaling_target_cpu_utilization" {
   type    = number
   default = 75
+  description = "Auto scaling cpu target"
 }
 
 variable "database_instance_type" {
@@ -28,49 +30,61 @@ variable "database_instance_type" {
 variable "database_instance_count" {
   type    = number
   default = 2
+  description = "Number of database instances"
 }
 
 variable "db_password_secret_arn" {
   type = string
+  description = "ARN of the Secrets Manager secret containing the database password"
 }
 
 variable "keycloak_password_secret_arn" {
   type = string
+  description = "ARN of the Secrets Manager secret containing the keycloak master password"
 }
 
 variable "keycloak_user" {
   type = string
+  description = "Username for the keycloak master user"
 }
 
 variable "tags" {
+  description = "Map of tags to set for the created resources"
 }
 
 variable "db_deletion_protection" {
   type    = bool
   default = true
+  description = "Prevent the database from being accidentally deleted"
 }
 
 variable "secrets_manager_kms_key_alias" {
   type = string
+  description = "Alias for the key used to decrypt password secrets"
 }
 
 variable "vpc_id" {
   type = string
+  description = "Id if the VPC"
 }
 
 variable "private_subnets" {
   type = list(string)
+  description = "List of subnet IDs"
 }
 
 variable "public_subnets" {
   type = list(string)
+  description = "List of subnet IDs"
 }
 
 variable "keycloak_url" {
   type = string
+  description = "FQDN for this keycloak instance. Used for creating internal certificates"
 }
 
 variable "keycloak_verbose_logging" {
   type    = bool
   default = false
+  description = "Enables verbose logging for the keycloak instances"
 }
